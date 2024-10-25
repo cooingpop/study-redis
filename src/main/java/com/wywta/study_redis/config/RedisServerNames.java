4c8aa0f728014d30d844015b4300bbadfe1fe0aa
cooingpop <cooingpop@gmail.com>
2024-10-25 14:56:54 +0900

    최초 프로젝트 초기화
    - 기본 셋팅, Redis 세팅 추가
    - Redis 테스트 추가

---
+++ src/main/java/com/wywta/study_redis/config/RedisServerNames.java
@@ -0,0 +1,5 @@
+package com.wywta.study_redis.config;
+
+public enum RedisServerNames {
+    QUEUE_CHAT
+}
---
+++ src/test/java/com/wywta/study_redis/StudyRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wywta.study_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
